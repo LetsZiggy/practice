LetsZiggy/practices

(jobpostr-adonisjs-aureliajs) add: SignIn/SignUp component

jobpostr-adonisjs-aureliajs/adonisjs/app/Controllers/Http/UserController.js
@@ -1,0 +1,115 @@
+"use strict"
+
+const User = use("App/Models/User")
+const Logger = use("Logger")
+
+Logger.level = "debug"
+
+class UserController {
+	async check ({ auth, request, response }) {
+		try {
+			const user = await auth
+				.getUser()
+
+			response
+				.ok({
+					data: {
+						isSignin: true,
+						userId: user.id,
+						username: user.username,
+					},
+				})
+		}
+		catch {
+			response
+				.badRequest({
+					data: {
+						isSignin: false,
+						userId: 0,
+						username: "",
+					},
+				})
+		}
+	}
+
+	async signin ({ auth, request, response }) {
+		try {
+			const { username, password } = request.all()
+
+			const user = await auth
+				.attempt(username, password)
+
+			response
+				.ok({
+					success: true,
+					data: {
+						isSignin: true,
+						userId: user.id,
+						username: user.username,
+					},
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("userSigninError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					data: {
+						isSignin: false,
+						userId: 0,
+						username: "",
+					},
+					errors: error,
+				})
+		}
+	}
+
+	async signout ({ auth, response }) {
+		await auth
+			.logout()
+
+		response
+			.ok({
+				success: true,
+			})
+	}
+
+	async signup ({ auth, request, response }) {
+		try {
+			const user = await User
+				.create(request.only([ "username", "email", "password" ]))
+
+			await auth
+				.login(user)
+
+			response
+				.created({
+					success: true,
+					data: {
+						isSignin: true,
+						userId: user.id,
+						username: user.username,
+					},
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("userSignupError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					data: {
+						isSignin: false,
+						userId: 0,
+						username: "",
+					},
+					errors: error,
+				})
+		}
+	}
+}
+
+module.exports = UserController

jobpostr-adonisjs-aureliajs/adonisjs/start/routes.js
@@ -17,7 +17,24 @@
 const Route = use("Route")
 
 /* API Routes */
-// PLACEHOLDER
+Route.group(() => {
+	Route
+		.get("users/check", "UserController.check")
+
+	Route
+		.post("users/signup", "UserController.signup")
+		.validator("UserSignup")
+		.middleware([ "guest" ])
+
+	Route
+		.post("users/signin", "UserController.signin")
+		.validator("UserSignin")
+		.middleware([ "guest" ])
+
+	Route
+		.post("users/signout", "UserController.signout")
+		.middleware([ "auth" ])
+}).prefix("api")
 
 /* Must be final route */
 Route.any("*", ({ view }) => view.render("index"))

LetsZiggy/practice

jobpostr-adonisjs-aureliajs/adonisjs/app/Controllers/Http/UserController.js
@@ -1,0 +1,115 @@
+"use strict"
+
+const User = use("App/Models/User")
+const Logger = use("Logger")
+
+Logger.level = "debug"
+
+class UserController {
+	async check ({ auth, request, response }) {
+		try {
+			const user = await auth
+				.getUser()
+
+			response
+				.ok({
+					data: {
+						isSignin: true,
+						userId: user.id,
+						username: user.username,
+					},
+				})
+		}
+		catch {
+			response
+				.badRequest({
+					data: {
+						isSignin: false,
+						userId: 0,
+						username: "",
+					},
+				})
+		}
+	}
+
+	async signin ({ auth, request, response }) {
+		try {
+			const { password, username } = request.all()
+
+			const user = await auth
+				.attempt(username, password)
+
+			response
+				.ok({
+					success: true,
+					data: {
+						isSignin: true,
+						userId: user.id,
+						username: user.username,
+					},
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("userSigninError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					data: {
+						isSignin: false,
+						userId: 0,
+						username: "",
+					},
+					errors: error,
+				})
+		}
+	}
+
+	async signout ({ auth, response }) {
+		await auth
+			.logout()
+
+		response
+			.ok({
+				success: true,
+			})
+	}
+
+	async signup ({ auth, request, response }) {
+		try {
+			const user = await User
+				.create(request.only(["username", "email", "password"]))
+
+			await auth
+				.login(user)
+
+			response
+				.created({
+					success: true,
+					data: {
+						isSignin: true,
+						userId: user.id,
+						username: user.username,
+					},
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("userSignupError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					data: {
+						isSignin: false,
+						userId: 0,
+						username: "",
+					},
+					errors: error,
+				})
+		}
+	}
+}
+
+module.exports = UserController

jobpostr-adonisjs-aureliajs/adonisjs/start/routes.js
@@ -17,7 +17,24 @@
 const Route = use("Route")
 
 /* API Routes */
-// PLACEHOLDER
+Route.group(() => {
+	Route
+		.get("users/check", "UserController.check")
+
+	Route
+		.post("users/signup", "UserController.signup")
+		.validator("UserSignup")
+		.middleware(["guest"])
+
+	Route
+		.post("users/signin", "UserController.signin")
+		.validator("UserSignin")
+		.middleware(["guest"])
+
+	Route
+		.post("users/signout", "UserController.signout")
+		.middleware(["auth"])
+}).prefix("api")
 
 /* Must be final route */
 Route.any("*", ({ view }) => view.render("index"))