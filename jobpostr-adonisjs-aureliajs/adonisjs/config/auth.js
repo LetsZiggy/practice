LetsZiggy/practices

(jobpostr-adonisjs-aureliajs) add: initialise adonisjs

jobpostr-adonisjs-aureliajs/adonisjs/config/auth.js
@@ -1,0 +1,94 @@
+"use strict"
+
+/** @type {import("@adonisjs/framework/src/Env")} */
+const Env = use("Env")
+
+module.exports = {
+	/*
+	|--------------------------------------------------------------------------
+	| Authenticator
+	|--------------------------------------------------------------------------
+	|
+	| Authentication is a combination of serializer and scheme with extra
+	| config to define on how to authenticate a user.
+	|
+	| Available Schemes - basic, session, jwt, api
+	| Available Serializers - lucid, database
+	|
+	*/
+	authenticator: "session",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Session
+	|--------------------------------------------------------------------------
+	|
+	| Session authenticator makes use of sessions to authenticate a user.
+	| Session authentication is always persistent.
+	|
+	*/
+	session: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "session",
+		uid: "username",
+		password: "password",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Basic Auth
+	|--------------------------------------------------------------------------
+	|
+	| The basic auth authenticator uses basic auth header to authenticate a
+	| user.
+	|
+	| NOTE:
+	| This scheme is not persistent and users are supposed to pass
+	| login credentials on each request.
+	|
+	*/
+	basic: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "basic",
+		uid: "username",
+		password: "password",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Jwt
+	|--------------------------------------------------------------------------
+	|
+	| The jwt authenticator works by passing a jwt token on each HTTP request
+	| via HTTP `Authorization` header.
+	|
+	*/
+	jwt: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "jwt",
+		uid: "username",
+		password: "password",
+		options: {
+			secret: Env.get("APP_KEY"),
+		},
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Api
+	|--------------------------------------------------------------------------
+	|
+	| The Api scheme makes use of API personal tokens to authenticate a user.
+	|
+	*/
+	api: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "api",
+		uid: "username",
+		password: "password",
+	},
+}

jobpostr-adonisjs-aureliajs/adonisjs/config/database.js
@@ -1,0 +1,81 @@
+"use strict"
+
+/** @type {import("@adonisjs/framework/src/Env")} */
+const Env = use("Env")
+
+/** @type {import("@adonisjs/ignitor/src/Helpers")} */
+const Helpers = use("Helpers")
+
+module.exports = {
+	/*
+	|--------------------------------------------------------------------------
+	| Default Connection
+	|--------------------------------------------------------------------------
+	|
+	| Connection defines the default connection settings to be used while
+	| interacting with SQL databases.
+	|
+	*/
+	connection: Env.get("DB_CONNECTION", "sqlite"),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Sqlite
+	|--------------------------------------------------------------------------
+	|
+	| Sqlite is a flat file database and can be good choice under development
+	| environment.
+	|
+	| npm i --save sqlite3
+	|
+	*/
+	sqlite: {
+		client: "sqlite3",
+		connection: {
+			filename: Helpers.databasePath(`${ Env.get("DB_DATABASE", "development") }.sqlite`),
+		},
+		useNullAsDefault: true,
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| MySQL
+	|--------------------------------------------------------------------------
+	|
+	| Here we define connection settings for MySQL database.
+	|
+	| npm i --save mysql
+	|
+	*/
+	mysql: {
+		client: "mysql",
+		connection: {
+			host: Env.get("DB_HOST", "localhost"),
+			port: Env.get("DB_PORT", ""),
+			user: Env.get("DB_USER", "root"),
+			password: Env.get("DB_PASSWORD", ""),
+			database: Env.get("DB_DATABASE", "adonis"),
+		},
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| PostgreSQL
+	|--------------------------------------------------------------------------
+	|
+	| Here we define connection settings for PostgreSQL database.
+	|
+	| npm i --save pg
+	|
+	*/
+	pg: {
+		client: "pg",
+		connection: {
+			host: Env.get("DB_HOST", "localhost"),
+			port: Env.get("DB_PORT", ""),
+			user: Env.get("DB_USER", "root"),
+			password: Env.get("DB_PASSWORD", ""),
+			database: Env.get("DB_DATABASE", "adonis"),
+		},
+	},
+}

jobpostr-adonisjs-aureliajs/adonisjs/config/session.js
@@ -1,0 +1,101 @@
+"use strict"
+
+/** @type {import("@adonisjs/framework/src/Env")} */
+const Env = use("Env")
+
+module.exports = {
+	/*
+	|--------------------------------------------------------------------------
+	| Session Driver
+	|--------------------------------------------------------------------------
+	|
+	| The session driver to be used for storing session values. It can be
+	| cookie, file or redis.
+	|
+	| For `redis` driver, make sure to install and register `@adonisjs/redis`
+	|
+	*/
+	driver: Env.get("SESSION_DRIVER", "cookie"),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Cookie Name
+	|--------------------------------------------------------------------------
+	|
+	| The name of the cookie to be used for saving session id. Session ids
+	| are signed and encrypted.
+	|
+	*/
+	cookieName: "adonis-session",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Clear session when browser closes
+	|--------------------------------------------------------------------------
+	|
+	| If this value is true, the session cookie will be temporary and will be
+	| removed when browser closes.
+	|
+	*/
+	clearWithBrowser: true,
+
+	/*
+	|--------------------------------------------------------------------------
+	| Session age
+	|--------------------------------------------------------------------------
+	|
+	| This value is only used when `clearWithBrowser` is set to false. The
+	| age must be a valid https://npmjs.org/package/ms string or should
+	| be in milliseconds.
+	|
+	| Valid values are:
+	|	"2h", "10d", "5y", "2.5 hrs"
+	|
+	*/
+	age: "2h",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Cookie options
+	|--------------------------------------------------------------------------
+	|
+	| Cookie options defines the options to be used for setting up session
+	| cookie
+	|
+	*/
+	cookie: {
+		httpOnly: true,
+		sameSite: "none",
+		path: "/",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Sessions location
+	|--------------------------------------------------------------------------
+	|
+	| If driver is set to file, we need to define the relative location from
+	| the temporary path or absolute url to any location.
+	|
+	*/
+	file: {
+		location: "sessions",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Redis config
+	|--------------------------------------------------------------------------
+	|
+	| The configuration for the redis driver. By default we reference it from
+	| the redis file. But you are free to define an object here too.
+	|
+	*/
+	redis: {
+		host: "127.0.0.1",
+		port: 6379,
+		password: null,
+		db: 0,
+		keyPrefix: "",
+	},
+}

LetsZiggy/practice

jobpostr-adonisjs-aureliajs/adonisjs/config/auth.js
@@ -1,0 +1,94 @@
+"use strict"
+
+/** @type {import("@adonisjs/framework/src/Env")} */
+const Environment = use("Env")
+
+module.exports = {
+	/*
+	|--------------------------------------------------------------------------
+	| Authenticator
+	|--------------------------------------------------------------------------
+	|
+	| Authentication is a combination of serializer and scheme with extra
+	| config to define on how to authenticate a user.
+	|
+	| Available Schemes - basic, session, jwt, api
+	| Available Serializers - lucid, database
+	|
+	*/
+	authenticator: "session",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Session
+	|--------------------------------------------------------------------------
+	|
+	| Session authenticator makes use of sessions to authenticate a user.
+	| Session authentication is always persistent.
+	|
+	*/
+	session: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "session",
+		uid: "username",
+		password: "password",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Basic Auth
+	|--------------------------------------------------------------------------
+	|
+	| The basic auth authenticator uses basic auth header to authenticate a
+	| user.
+	|
+	| NOTE:
+	| This scheme is not persistent and users are supposed to pass
+	| login credentials on each request.
+	|
+	*/
+	basic: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "basic",
+		uid: "username",
+		password: "password",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Jwt
+	|--------------------------------------------------------------------------
+	|
+	| The jwt authenticator works by passing a jwt token on each HTTP request
+	| via HTTP `Authorization` header.
+	|
+	*/
+	jwt: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "jwt",
+		uid: "username",
+		password: "password",
+		options: {
+			secret: Environment.get("APP_KEY"),
+		},
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Api
+	|--------------------------------------------------------------------------
+	|
+	| The Api scheme makes use of API personal tokens to authenticate a user.
+	|
+	*/
+	api: {
+		serializer: "lucid",
+		model: "App/Models/User",
+		scheme: "api",
+		uid: "username",
+		password: "password",
+	},
+}

jobpostr-adonisjs-aureliajs/adonisjs/config/database.js
@@ -1,0 +1,81 @@
+"use strict"
+
+/** @type {import("@adonisjs/framework/src/Env")} */
+const Environment = use("Env")
+
+/** @type {import("@adonisjs/ignitor/src/Helpers")} */
+const Helpers = use("Helpers")
+
+module.exports = {
+	/*
+	|--------------------------------------------------------------------------
+	| Default Connection
+	|--------------------------------------------------------------------------
+	|
+	| Connection defines the default connection settings to be used while
+	| interacting with SQL databases.
+	|
+	*/
+	connection: Environment.get("DB_CONNECTION", "sqlite"),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Sqlite
+	|--------------------------------------------------------------------------
+	|
+	| Sqlite is a flat file database and can be good choice under development
+	| environment.
+	|
+	| npm i --save sqlite3
+	|
+	*/
+	sqlite: {
+		client: "sqlite3",
+		connection: {
+			filename: Helpers.databasePath(`${ Environment.get("DB_DATABASE", "development") }.sqlite`),
+		},
+		useNullAsDefault: true,
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| MySQL
+	|--------------------------------------------------------------------------
+	|
+	| Here we define connection settings for MySQL database.
+	|
+	| npm i --save mysql
+	|
+	*/
+	mysql: {
+		client: "mysql",
+		connection: {
+			host: Environment.get("DB_HOST", "localhost"),
+			port: Environment.get("DB_PORT", ""),
+			user: Environment.get("DB_USER", "root"),
+			password: Environment.get("DB_PASSWORD", ""),
+			database: Environment.get("DB_DATABASE", "adonis"),
+		},
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| PostgreSQL
+	|--------------------------------------------------------------------------
+	|
+	| Here we define connection settings for PostgreSQL database.
+	|
+	| npm i --save pg
+	|
+	*/
+	pg: {
+		client: "pg",
+		connection: {
+			host: Environment.get("DB_HOST", "localhost"),
+			port: Environment.get("DB_PORT", ""),
+			user: Environment.get("DB_USER", "root"),
+			password: Environment.get("DB_PASSWORD", ""),
+			database: Environment.get("DB_DATABASE", "adonis"),
+		},
+	},
+}

jobpostr-adonisjs-aureliajs/adonisjs/config/session.js
@@ -1,0 +1,102 @@
+"use strict"
+
+/** @type {import("@adonisjs/framework/src/Env")} */
+const Environment = use("Env")
+
+module.exports = {
+	/*
+	|--------------------------------------------------------------------------
+	| Session Driver
+	|--------------------------------------------------------------------------
+	|
+	| The session driver to be used for storing session values. It can be
+	| cookie, file or redis.
+	|
+	| For `redis` driver, make sure to install and register `@adonisjs/redis`
+	|
+	*/
+	driver: Environment.get("SESSION_DRIVER", "cookie"),
+
+	/*
+	|--------------------------------------------------------------------------
+	| Cookie Name
+	|--------------------------------------------------------------------------
+	|
+	| The name of the cookie to be used for saving session id. Session ids
+	| are signed and encrypted.
+	|
+	*/
+	cookieName: "adonis-session",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Clear session when browser closes
+	|--------------------------------------------------------------------------
+	|
+	| If this value is true, the session cookie will be temporary and will be
+	| removed when browser closes.
+	|
+	*/
+	clearWithBrowser: true,
+
+	/*
+	|--------------------------------------------------------------------------
+	| Session age
+	|--------------------------------------------------------------------------
+	|
+	| This value is only used when `clearWithBrowser` is set to false. The
+	| age must be a valid https://npmjs.org/package/ms string or should
+	| be in milliseconds.
+	|
+	| Valid values are:
+	|	"2h", "10d", "5y", "2.5 hrs"
+	|
+	*/
+	age: "2h",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Cookie options
+	|--------------------------------------------------------------------------
+	|
+	| Cookie options defines the options to be used for setting up session
+	| cookie
+	|
+	*/
+	cookie: {
+		httpOnly: true,
+		sameSite: "none",
+		secure: true,
+		path: "/",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Sessions location
+	|--------------------------------------------------------------------------
+	|
+	| If driver is set to file, we need to define the relative location from
+	| the temporary path or absolute url to any location.
+	|
+	*/
+	file: {
+		location: "sessions",
+	},
+
+	/*
+	|--------------------------------------------------------------------------
+	| Redis config
+	|--------------------------------------------------------------------------
+	|
+	| The configuration for the redis driver. By default we reference it from
+	| the redis file. But you are free to define an object here too.
+	|
+	*/
+	redis: {
+		host: "127.0.0.1",
+		port: 6379,
+		password: null,
+		db: 0,
+		keyPrefix: "",
+	},
+}