ea9ff46b0cabfc7c1e1c2fd2168fd3d9b56883a6
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2025-02-21 17:31:10 +0800

    (wiki-go-std) add: basic save/load wiki

--- wiki-go-std/wiki.go
+++ wiki-go-std/wiki.go
@@ -1,1 +1,36 @@
 package main
+
+import (
+	fmt "fmt"
+	log "log"
+	os "os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (page *Page) save() error {
+	filename := page.Title + ".txt"
+	return os.WriteFile(filename, page.Body, 0o600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	pageTest1 := &Page{Title: "TestPage", Body: []byte("This is a test page.")}
+	pageTest1.save()
+	pageTest2, err := loadPage("TestPage")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(pageTest2.Body))
+}
