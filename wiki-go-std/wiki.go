dccaf92b7d94538b37ff60b6fd8fa543bdeba8c2
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2025-02-21 17:31:10 +0800

    (wiki-go-std) add: basic functionality

--- wiki-go-std/wiki.go
+++ wiki-go-std/wiki.go
@@ -1,1 +1,108 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	html "html/template"
+	"log"
+	"net/http"
+	"os"
+	re "regexp"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+var (
+	templates = html.Must(html.ParseFiles("edit.html", "view.html"))
+	validPath = re.MustCompile("^/((?:edit)|(?:save)|(?:view))/([a-zA-Z0-9]+)$")
+)
+
+func (page *Page) save() error {
+	filename := page.Title + ".txt"
+	return os.WriteFile(filename, page.Body, 0o600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Page{Title: title, Body: body}, nil
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, page *Page) {
+	err := templates.ExecuteTemplate(w, tmpl+".html", page)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+	match := validPath.FindStringSubmatch(r.URL.Path)
+	if match == nil {
+		http.NotFound(w, r)
+
+		return "", errors.New("invalid page title")
+	}
+
+	return match[2], nil
+}
+
+func makeHandler(fn func(http.ResponseWriter, *http.Request, string)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		match := validPath.FindStringSubmatch(r.URL.Path)
+		if match == nil {
+			http.NotFound(w, r)
+
+			return
+		}
+
+		fn(w, r, match[2])
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+	page, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+
+		return
+	}
+
+	renderTemplate(w, "view", page)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+	page, err := loadPage(title)
+	if err != nil {
+		page = &Page{Title: title}
+	}
+
+	renderTemplate(w, "edit", page)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+	body := r.FormValue("body")
+	page := &Page{Title: title, Body: []byte(body)}
+
+	err := page.save()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	http.HandleFunc("/view/", makeHandler(viewHandler))
+	http.HandleFunc("/edit/", makeHandler(editHandler))
+	http.HandleFunc("/save/", makeHandler(saveHandler))
+
+	fmt.Println("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
