195f7a41baffe46ad5a614e232db5c3f2795f302
Shafiq Tahir <shafiqtahir.khan@gmail.com>
2025-02-22 16:43:25 +0800

    (wiki-go-std) add: combine load page with net/http

--- wiki-go-std/wiki.go
+++ wiki-go-std/wiki.go
@@ -4,6 +4,7 @@ import (
 	fmt "fmt"
 	log "log"
 	http "net/http"
+	os "os"
 )
 
 type Page struct {
@@ -11,14 +12,33 @@ type Page struct {
 	Body  []byte
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "r.URL.Path %s", r.URL.Path)
-	fmt.Fprintf(w, "\n")
-	fmt.Fprintf(w, "r.URL.Path[1:] %s", r.URL.Path[1:])
+func (page *Page) save() error {
+	filename := page.Title + ".txt"
+	return os.WriteFile(filename, page.Body, 0o600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	page, err := loadPage(title)
+	if err != nil {
+		log.Println(err)
+		page = &Page{Title: "No Such Page", Body: []byte("~~~NIL~~~")}
+	}
+
+	fmt.Fprintf(w, "<h3>%s</h3><p>%s</p>", page.Title, page.Body)
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	fmt.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
