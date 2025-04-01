LetsZiggy/practices

(jobpostr-adonisjs-aureliajs) add: Jobs component

jobpostr-adonisjs-aureliajs/adonisjs/app/Controllers/Http/JobController.js
@@ -1,0 +1,115 @@
+"use strict"
+
+const Job = use("App/Models/Job")
+const Logger = use("Logger")
+
+Logger.level = "debug"
+
+class JobController {
+	async deleteJob ({ auth, request, response }) {
+		try {
+			await auth
+				.user
+				.jobs()
+				.where("id", request.input("id", 0))
+				.delete()
+
+			response
+				.ok({
+					success: true,
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobDeleteError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+
+	async editJob ({ auth, request, response }) {
+		try {
+			await auth
+				.user
+				.jobs()
+				.where("id", request.input("id", 0))
+				.update(request.only([ "title", "link", "description" ]))
+
+			response
+				.ok({
+					success: true,
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobEditError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+
+	async list ({ request, response }) {
+		try {
+			const t = Number(request.input("t", 0))
+			const jobs = (t === 0)
+				? await Job
+					.all()
+				: await Job
+					.query()
+					.where("updated_at", ">", new Date(t))
+					.fetch()
+
+			response
+				.ok({
+					success: true,
+					data: {
+						jobs,
+					},
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobAllError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+
+	async newJob ({ auth, request, response }) {
+		try {
+			await auth
+				.user
+				.jobs()
+				.create(request.only([ "title", "link", "description" ]))
+
+			response
+				.ok({
+					success: true,
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobCreateError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+}
+
+module.exports = JobController

jobpostr-adonisjs-aureliajs/aureliajs/src/store-actions.js
@@ -1,4 +1,4 @@
-/* eslint-disable unicorn/no-array-for-each */
+/* eslint-disable unicorn/no-object-as-default-parameter, unicorn/no-null, unicorn/no-array-for-each */
 
 import { nextStateHistory } from "aurelia-store"
 
@@ -45,6 +45,104 @@ const actions = {
 		return nextStateHistory(state, {
 			...state.present,
 			userId: data,
+		})
+	},
+	setJobs: (state, { data = [], type = "full" }) => {
+		let jobs = null
+
+		if (type === "full") {
+			jobs = [ ...data ]
+		}
+		else {
+			const currentJobs = [ ...state.present.jobs ]
+			const newJobs = []
+
+			for (let index = (data.length - 1); index >= 0; index--) {
+				let found = false
+
+				for (let index_ = (currentJobs.length - 1); index_ >= 0; index_--) {
+					if (currentJobs[index_].id === data[index].id) {
+						found = true
+						currentJobs[index_] = { ...data[index] }
+						break
+					}
+				}
+
+				if (!found) {
+					newJobs.push(data[index])
+				}
+			}
+
+			jobs = [ ...currentJobs, ...newJobs ]
+		}
+
+		return nextStateHistory(state, {
+			...state.present,
+			jobs,
+		})
+	},
+	setJobsLastUpdate: (state) => {
+		return nextStateHistory(state, {
+			...state.present,
+			jobsLastUpdate: Date.now(),
+		})
+	},
+	updateJob: (state, data = { id: 0 }) => {
+		const jobs = [ ...state.present.jobs ]
+		const index = jobs
+			.reduce((accumulator, value, index) => {
+				return ((value.id === data.id) ? index : accumulator)
+			}, -1)
+
+		if (index === -1) {
+			return nextStateHistory(state, {
+				...state.present,
+			})
+		}
+
+		jobs[index] = { ...jobs[index], ...data }
+
+		return nextStateHistory(state, {
+			...state.present,
+			jobs,
+		})
+	},
+	deleteJob: (state, data = { id: 0 }) => {
+		const jobs = [ ...state.present.jobs ]
+		const index = jobs
+			.reduce((accumulator, value, index) => {
+				return ((value.id === data.id) ? index : accumulator)
+			}, -1)
+
+		if (index === -1) {
+			return nextStateHistory(state, {
+				...state.present,
+			})
+		}
+
+		jobs.splice(index, 1)
+
+		return nextStateHistory(state, {
+			...state.present,
+			jobs,
+		})
+	},
+	toggleJobTitleValid: (state, data = false) => {
+		return nextStateHistory(state, {
+			...state.present,
+			isJobTitleValid: data,
+		})
+	},
+	toggleJobLinkValid: (state, data = false) => {
+		return nextStateHistory(state, {
+			...state.present,
+			isJobLinkValid: data,
+		})
+	},
+	toggleJobDescriptionValid: (state, data = false) => {
+		return nextStateHistory(state, {
+			...state.present,
+			isJobDescriptionValid: data,
 		})
 	},
 	// action: (state, data) => {

LetsZiggy/practice

jobpostr-adonisjs-aureliajs/adonisjs/app/Controllers/Http/JobController.js
@@ -1,0 +1,115 @@
+"use strict"
+
+const Job = use("App/Models/Job")
+const Logger = use("Logger")
+
+Logger.level = "debug"
+
+class JobController {
+	async deleteJob ({ auth, request, response }) {
+		try {
+			await auth
+				.user
+				.jobs()
+				.where("id", request.input("id", 0))
+				.delete()
+
+			response
+				.ok({
+					success: true,
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobDeleteError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+
+	async editJob ({ auth, request, response }) {
+		try {
+			await auth
+				.user
+				.jobs()
+				.where("id", request.input("id", 0))
+				.update(request.only(["title", "link", "description"]))
+
+			response
+				.ok({
+					success: true,
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobEditError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+
+	async list ({ request, response }) {
+		try {
+			const t = Number(request.input("t", 0))
+			const jobs = (t === 0)
+				? await Job
+					.all()
+				: await Job
+					.query()
+					.where("updated_at", ">", new Date(t))
+					.fetch()
+
+			response
+				.ok({
+					success: true,
+					data: {
+						jobs,
+					},
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobAllError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+
+	async newJob ({ auth, request, response }) {
+		try {
+			await auth
+				.user
+				.jobs()
+				.create(request.only(["title", "link", "description"]))
+
+			response
+				.ok({
+					success: true,
+				})
+		}
+		catch (error) {
+			Logger
+				.debug("jobCreateError: %j", error)
+
+			response
+				.badRequest({
+					success: false,
+					errors: error,
+				})
+		}
+	}
+}
+
+module.exports = JobController

jobpostr-adonisjs-aureliajs/aureliajs/src/store-actions.js
@@ -1,4 +1,4 @@
-/* eslint-disable unicorn/no-array-for-each */
+/* eslint-disable unicorn/no-object-as-default-parameter, unicorn/no-null, unicorn/no-array-for-each */
 
 import { nextStateHistory } from "aurelia-store"
 
@@ -45,6 +45,105 @@ const actions = {
 		return nextStateHistory(state, {
 			...state.present,
 			userId: data,
+		})
+	},
+	setJobs: (state, { data = [], type = "full" }) => {
+		let jobs = null
+
+		if (type === "full") {
+			jobs = [...data]
+		}
+		else {
+			const currentJobs = [...state.present.jobs]
+			const newJobs = []
+
+			for (let index = (data.length - 1); index >= 0; index--) {
+				let found = false
+
+				for (let index_ = (currentJobs.length - 1); index_ >= 0; index_--) {
+					if (currentJobs[index_].id === data[index].id) {
+						found = true
+						currentJobs[index_] = { ...data[index] }
+
+						break
+					}
+				}
+
+				if (!found) {
+					newJobs.push(data[index])
+				}
+			}
+
+			jobs = [...currentJobs, ...newJobs]
+		}
+
+		return nextStateHistory(state, {
+			...state.present,
+			jobs,
+		})
+	},
+	setJobsLastUpdate: (state) => {
+		return nextStateHistory(state, {
+			...state.present,
+			jobsLastUpdate: Date.now(),
+		})
+	},
+	updateJob: (state, data = { id: 0 }) => {
+		const jobs = [...state.present.jobs]
+		const index = jobs
+			.reduce((accumulator, value, index) => {
+				return ((value.id === data.id) ? index : accumulator)
+			}, -1)
+
+		if (index === -1) {
+			return nextStateHistory(state, {
+				...state.present,
+			})
+		}
+
+		jobs[index] = { ...jobs[index], ...data }
+
+		return nextStateHistory(state, {
+			...state.present,
+			jobs,
+		})
+	},
+	deleteJob: (state, data = { id: 0 }) => {
+		const jobs = [...state.present.jobs]
+		const index = jobs
+			.reduce((accumulator, value, index) => {
+				return ((value.id === data.id) ? index : accumulator)
+			}, -1)
+
+		if (index === -1) {
+			return nextStateHistory(state, {
+				...state.present,
+			})
+		}
+
+		jobs.splice(index, 1)
+
+		return nextStateHistory(state, {
+			...state.present,
+			jobs,
+		})
+	},
+	toggleJobTitleValid: (state, data = false) => {
+		return nextStateHistory(state, {
+			...state.present,
+			isJobTitleValid: data,
+		})
+	},
+	toggleJobLinkValid: (state, data = false) => {
+		return nextStateHistory(state, {
+			...state.present,
+			isJobLinkValid: data,
+		})
+	},
+	toggleJobDescriptionValid: (state, data = false) => {
+		return nextStateHistory(state, {
+			...state.present,
+			isJobDescriptionValid: data,
 		})
 	},
 	// action: (state, data) => {