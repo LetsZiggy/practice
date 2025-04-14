LetsZiggy/practices

(jobpostr-adonisjs-aureliajs) add: CSRF for /api routes

jobpostr-adonisjs-aureliajs/adonisjs/start/routes.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const Token = use("App/Models/Token")
+
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -19,7 +21,10 @@ const Route = use("Route")
 /* API Routes */
 Route.group(() => {
 	Route
-		.get("users/check", "UserController.check")
+		.post("users/check", "UserController.check")
+
+	Route
+		.post("users/end-session", "UserController.endSession")
 
 	Route
 		.post("users/signup", "UserController.signup")
@@ -41,18 +46,26 @@ Route.group(() => {
 	Route
 		.post("jobs/new-job", "JobController.newJob")
 		.validator("Job")
-		.middleware([ "auth" ])
+		.middleware([ "auth", "apiToken" ])
 
 	Route
 		.post("jobs/edit-job", "JobController.editJob")
 		.validator("Job")
-		.middleware([ "auth" ])
+		.middleware([ "auth", "apiToken" ])
 
 	Route
 		.post("jobs/delete-job", "JobController.deleteJob")
 		.validator("Delete")
-		.middleware([ "auth" ])
+		.middleware([ "auth", "apiToken" ])
 }).prefix("api")
 
 /* Must be final route */
-Route.any("*", ({ view }) => view.render("index"))
+Route.any("*", async ({ view, request }) => {
+	await Token
+		.create({
+			identifier: request.nonce,
+			token: request.csrfToken,
+		})
+
+	return view.render("index")
+})

LetsZiggy/practice

jobpostr-adonisjs-aureliajs/adonisjs/start/routes.js
@@ -1,5 +1,7 @@
 "use strict"
 
+const Token = use("App/Models/Token")
+
 /*
 |--------------------------------------------------------------------------
 | Routes
@@ -19,7 +21,10 @@ const Route = use("Route")
 /* API Routes */
 Route.group(() => {
 	Route
-		.get("users/check", "UserController.check")
+		.post("users/check", "UserController.check")
+
+	Route
+		.post("users/end-session", "UserController.endSession")
 
 	Route
 		.post("users/signup", "UserController.signup")
@@ -41,18 +46,26 @@ Route.group(() => {
 	Route
 		.post("jobs/new-job", "JobController.newJob")
 		.validator("Job")
-		.middleware(["auth"])
+		.middleware(["auth", "apiToken"])
 
 	Route
 		.post("jobs/edit-job", "JobController.editJob")
 		.validator("Job")
-		.middleware(["auth"])
+		.middleware(["auth", "apiToken"])
 
 	Route
 		.post("jobs/delete-job", "JobController.deleteJob")
 		.validator("Delete")
-		.middleware(["auth"])
+		.middleware(["auth", "apiToken"])
 }).prefix("api")
 
 /* Must be final route */
-Route.any("*", ({ view }) => view.render("index"))
+Route.any("*", async ({ request, view }) => {
+	await Token
+		.create({
+			identifier: request.nonce,
+			token: request.csrfToken,
+		})
+
+	return view.render("index")
+})