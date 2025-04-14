LetsZiggy/practices

(jobpostr-adonisjs-aureliajs) add: initialise aureliajs

jobpostr-adonisjs-aureliajs/aureliajs/e2e/app.spec.js
@@ -1,0 +1,12 @@
+const { test, expect } = require("@playwright/test")
+
+test.beforeEach(async ({ page }) => {
+	await page.goto("http://localhost:9000")
+})
+
+test.describe("app", () => {
+	test("shows message", async ({ page }) => {
+		await expect(page.locator("h1"))
+			.toHaveText("Hello World!")
+	})
+})

jobpostr-adonisjs-aureliajs/aureliajs/src/store-middlewares.js
@@ -1,0 +1,57 @@
+/* eslint-disable unicorn/no-array-for-each */
+
+import { MiddlewarePlacement } from "aurelia-store"
+
+const middlewares = {
+// 	middleware: (currentState, originalState, settings, action) => {},
+}
+
+const getMiddleware = (keyArray, middleware = middlewares) => {
+	const maxIndex = keyArray.length - 1
+
+	middleware = (maxIndex === 0)
+		? middleware[keyArray[0]]
+		: getMiddleware(keyArray.slice(1), middleware[keyArray[0]])
+
+	return middleware
+}
+
+// store.registerMiddleware(middleware, MiddlewarePlacement[placement], options)
+export const registerMiddlewares = (store, list) => {
+	list
+		.map(({ key, placement, options }) => {
+			placement = placement
+				.toLowerCase()
+				.replaceAll(/(^[a-z]| [a-z])/g, (match) => match.toUpperCase())
+
+			const middleware = (typeof key === "string")
+				? getMiddleware(key.split("."))
+				: key
+
+			return ({
+				middleware,
+				placement,
+				options,
+			})
+		})
+		.forEach(({ middleware, placement, options }) => {
+			store.registerMiddleware(middleware, MiddlewarePlacement[placement], options)
+		})
+}
+
+// store.unregisterMiddleware(middleware)
+export const unregisterMiddlewares = (store, list) => {
+	list
+		.map(({ key }) => {
+			const middleware = (typeof key === "string")
+				? getMiddleware(key.split("."))
+				: key
+
+			return ({
+				middleware,
+			})
+		})
+		.forEach(({ middleware }) => {
+			store.unregisterMiddleware(middleware)
+		})
+}

LetsZiggy/practice

jobpostr-adonisjs-aureliajs/aureliajs/e2e/app.spec.js
@@ -1,0 +1,12 @@
+const { expect, test } = require("@playwright/test")
+
+test.beforeEach(async ({ page }) => {
+	await page.goto("http://localhost:9000")
+})
+
+test.describe("app", () => {
+	test("shows message", async ({ page }) => {
+		await expect(page.locator("h1"))
+			.toHaveText("Hello World!")
+	})
+})

jobpostr-adonisjs-aureliajs/aureliajs/src/store-middlewares.js
@@ -1,0 +1,57 @@
+/* eslint-disable unicorn/no-array-for-each */
+
+import { MiddlewarePlacement } from "aurelia-store"
+
+const middlewares = {
+// 	middleware: (currentState, originalState, settings, action) => {},
+}
+
+const getMiddleware = (keyArray, middleware = middlewares) => {
+	const maxIndex = keyArray.length - 1
+
+	middleware = (maxIndex === 0)
+		? middleware[keyArray[0]]
+		: getMiddleware(keyArray.slice(1), middleware[keyArray[0]])
+
+	return middleware
+}
+
+// store.registerMiddleware(middleware, MiddlewarePlacement[placement], options)
+export const registerMiddlewares = (store, list) => {
+	list
+		.map(({ key, options, placement }) => {
+			placement = placement
+				.toLowerCase()
+				.replaceAll(/(^[a-z]| [a-z])/g, (match) => match.toUpperCase())
+
+			const middleware = (typeof key === "string")
+				? getMiddleware(key.split("."))
+				: key
+
+			return ({
+				middleware,
+				placement,
+				options,
+			})
+		})
+		.forEach(({ middleware, options, placement }) => {
+			store.registerMiddleware(middleware, MiddlewarePlacement[placement], options)
+		})
+}
+
+// store.unregisterMiddleware(middleware)
+export const unregisterMiddlewares = (store, list) => {
+	list
+		.map(({ key }) => {
+			const middleware = (typeof key === "string")
+				? getMiddleware(key.split("."))
+				: key
+
+			return ({
+				middleware,
+			})
+		})
+		.forEach(({ middleware }) => {
+			store.unregisterMiddleware(middleware)
+		})
+}